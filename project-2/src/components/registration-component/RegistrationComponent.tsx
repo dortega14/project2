5e9ec04c711b2a974f3ccf6f8219d953c21c2887
rhlaroya <rhlaroya@gmail.com>
2020-02-14 20:37:33 -0500

    finished RegistrationComponent

--- project-2/src/components/registration-component/RegistrationComponent.tsx
+++ project-2/src/components/registration-component/RegistrationComponent.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../css/Register.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import {Redirect, NavLink} from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { regUser } from '../../utilities/api';
 
-function Register() {
+export const Register:React.FC<any> = (props:any) => {
+    const [email, setemail] = useState("");
+    const [username, setuname] = useState("");
+    const [password, setpass] = useState("");
+    const [first_name, setfirst] = useState("");
+    const [last_name, setlast] = useState("");
+    
     return(
         <div >
             <header className="MainButtons">
@@ -22,7 +29,14 @@ function Register() {
         </header>
 
         <div className="container">
-            <form id ="RegisterForm" className="form-horizontal">
+            <form id ="RegisterForm" className="form-horizontal" onSubmit={()=>regUser({
+                user_id:0,
+                email: email,
+                username: username,
+                password: password,
+                first_name: first_name,
+                last_name: last_name
+            }).then(r=>console.log(r.data))}>
                 <div id="RegisterElements" className="row">
                 <div className="TextForRegister">
                     <p>Indulge in an exciting experence </p>
@@ -31,26 +45,26 @@ function Register() {
                 </div>
                 <div id="FirstName" className="form-group col-md-4">
                         <label>First Name</label>
-                        <input className="form-control input-lg" placeholder="first name" /> 
+                        <input className="form-control input-lg" placeholder="first name" onChange={val=>setfirst(val.target.value)}/> 
                 </div>
                 <div className="form-group col-md-4">
                         <label>Last Name</label>
-                        <input className="form-control input-lg" placeholder="last name" /> 
+                        <input className="form-control input-lg" placeholder="last name" onChange={val=>setlast(val.target.value)}/> 
                 </div>
                 <div className="form-group col-md-4">
                         <label>User Name</label>
-                        <input className="form-control input-lg" placeholder="user name" /> 
+                        <input className="form-control input-lg" placeholder="user name" onChange={val=>setuname(val.target.value)}/> 
                     </div>
                     <div className="form-group col-md-4">
                         <label htmlFor="exampleInputEmail1">Email address </label>
-                        <input type="email" className="form-control input-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <input type="email" className="form-control input-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={val=>setemail(val.target.value)}/>
                         <small id="emailHelp" className="form-text text-muted"></small>
                     </div>
                     
                     <div className="form-group col-md-4">
                         <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control input-lg" id="exampleInputPassword1" placeholder="Password" /> 
-                        <button id="SubmitButton" type="submit" className="btn btn-primary">Submit</button>
+                        <input type="password" className="form-control input-lg" id="exampleInputPassword1" placeholder="Password" onChange={val=>setpass(val.target.value)}/> 
+                        <button id="SubmitButton" type="submit" className="btn btn-primary">Sign Up</button>
                     </div> 
                 </div>
     
