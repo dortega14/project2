62f1b2779b3f865ff47f8a21ca7d1c63445417be
rhlaroya <rhlaroya@gmail.com>
2020-02-20 21:31:20 -0500

    adding JavaDocs

--- project2/src/main/java/com/revature/dao/CategoryDao.java
+++ project2/src/main/java/com/revature/dao/CategoryDao.java
@@ -11,42 +11,77 @@ import com.revature.model.Category;
 
 /**
  * @author TeamLeo
- * Contains the CRUD
+ * Contains the CRUD methods for the Category Model.
  */
 @Repository
 @Transactional
 public class CategoryDao {
 
+	/**
+	 * SessionFactory Initialization.
+	 */
 	private SessionFactory sesfact;
 	
+	/**
+	 * Automatically creates association to CategoryDao
+	 * @param sesfact
+	 */
 	@Autowired
 	public CategoryDao(SessionFactory sesfact) {
 		super();
 		this.sesfact = sesfact;
 	}
 
+	/**
+	 * Finds all the categories
+	 * @return category
+	 */
 	public List<Category> findAll() {
 		return sesfact.getCurrentSession().createQuery("from Category", Category.class).list();
 	}
 
+	/**
+	 * Finds categories by id
+	 * @param id
+	 * @return category
+	 */
 	public Category findById(int id) {
 		return sesfact.getCurrentSession().get(Category.class, id);
 	}
 
+	/**
+	 * Updates Category
+	 * @param t
+	 * @return
+	 */
 	public Category update(Category t) {
 		sesfact.getCurrentSession().update(t);
 		return t;
 	}
 
+	/**
+	 * Inserts a Category
+	 * @param t
+	 * @return t
+	 */
 	public Category insert(Category t) {
 		sesfact.getCurrentSession().save(t);
 		return t;
 	}
 
+	/**
+	 * Deletes a Category
+	 * @param t
+	 */
 	public void delete(Category t) {
 		sesfact.getCurrentSession().delete(t);
 	}
 
+	/**
+	 * Finds Category by name
+	 * @param name
+	 * @return null
+	 */
 	public Category findByName(String name) {
 		return null;
 	}
--- project2/src/main/java/com/revature/service/CategoryService.java
+++ project2/src/main/java/com/revature/service/CategoryService.java
@@ -8,32 +8,67 @@ import org.springframework.stereotype.Service;
 import com.revature.dao.CategoryDao;
 import com.revature.model.Category;
 
+/**
+ * @author TeamLeo
+ * Service layer for the Category model.
+ */
 @Service
 public class CategoryService {
 	
+	/**
+	 * CategoryDao Instantiation.
+	 */
 	private CategoryDao cd;
 
+	/**
+	 * Automatically creates associations for the 
+	 * Category.
+	 * @param cd
+	 */
 	@Autowired
 	public void setCd(CategoryDao cd) {
 		this.cd = cd;
 	}
 
+	/**
+	 * 
+	 * @return cd.findAll()
+	 */
 	public List<Category> readAll() {
 		return cd.findAll();
 	}
 	
+	/**
+	 * 
+	 * @param id
+	 * @return findById(id)
+	 */
 	public Category findById(int id) {
 		return cd.findById(id);
 	}
 	
+	/**
+	 * 
+	 * @param cat
+	 * @return update(cat)
+	 */
 	public Category update(Category cat) {
 		return cd.update(cat);
 	}
 	
+	/**
+	 * 
+	 * @param cat
+	 * @return insert
+	 */
 	public Category insert(Category cat) {
 		return cd.insert(cat);
 	}
 	
+	/**
+	 * 
+	 * @param cat
+	 */
 	public void delete(Category cat) {
 		cd.delete(cat);
 	}
